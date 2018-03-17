8062adcf142306f556bd90047ac321273f73d375
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2018-03-17 18:48:54 +0100

    First failing method in HMMTest.

---
+++ src/test/java/belfern/uji/es/hmm/HMMTest.java
@@ -0,0 +1,12 @@
+package belfern.uji.es.hmm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class HMMTest {
+    @Test
+    public void instantiateNodeTest() {
+        fail("This method should fail");
+    }
+}
