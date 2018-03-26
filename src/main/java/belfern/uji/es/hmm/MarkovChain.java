ec376b22acbcdaa400dc945ae033c9f2eb2d6585
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2018-03-26 18:06:39 +0200

    Some comments removed.

--- src/main/java/belfern/uji/es/hmm/MarkovChain.java
+++ src/main/java/belfern/uji/es/hmm/MarkovChain.java
@@ -7,17 +7,11 @@ public class MarkovChain<T> extends HMM<T, T> {
 
     @Override
     public Node<T, T> instanceInitialNode(T id, Emitter<T> emitter) {
-//        initialNode = instanceNode(id, () -> id);
-//        nodes.put(id, initialNode);
-//        return initialNode;
         return super.instanceInitialNode(id, emitter);
     }
 
     @Override
     public Node<T, T> instanceNode(T id, Emitter<T> emitter) {
-//        Node<T, T> node = new Node<T, T>(id, emitter);
-//        nodes.put(id, node);
-//        return node;
         return super.instanceNode(id, emitter);
     }
 
